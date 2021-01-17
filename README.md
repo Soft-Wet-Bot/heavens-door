e0774cf15cf5aee80f5979a8738fb6af2cd138e3
Evangelos Paterakis <evan@geopjr.dev>
2021-01-17 06:22:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <img alt="heavensdoor branding" src="https://i.imgur.com/xFHOInK.png">
+</p>
+
+# Soft & Wet Heavens Door
+
+This is a module for [Soft & Wet](https://github.com/Soft-Wet-Bot/base).
+
+## Contributors
+
+<a href="https://github.com/Soft-Wet-Bot/heavens-door/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=Soft-Wet-Bot/heavens-door" />
+</a>
+
+Made with [contributors-img](https://contrib.rocks).
+
+The same image can be found in the bot's info command!
+
+### Don't forget to read to read [CONTRIBUTING.md](https://github.com/Soft-Wet-Bot/heavens-door/blob/main/CONTRIBUTING.md) before doing so.
---
+++ index.js
@@ -0,0 +1,87 @@
+import BaseModule from "./structures/BaseModule.js";
+
+import fetch from "node-fetch";
+import fs from "fs";
+import gm from "gm";
+import sharp from "sharp";
+
+export default class HeavensDoor extends BaseModule {
+  /**
+   * @param {Main} main
+   */
+  constructor(main) {
+    super(main);
+
+    this.fs = fs;
+    this.gm = gm.subClass({ imageMagick: true });
+    this.sharp = sharp;
+
+    this.register(HeavensDoor, {
+      name: "heavensdoor",
+    });
+  }
+
+  download(uri, filename) {
+    return new Promise(async (resolve, reject) => {
+      const res = await fetch(uri);
+      const fileStream = fs.createWriteStream(filename);
+
+      res.body.pipe(fileStream);
+      res.body.on("error", reject);
+      fileStream.on("finish", resolve);
+    });
+  }
+
+  getAssetPath(name) {
+    return process.cwd() + "/assets/images/" + name;
+  }
+
+  toPng(image) {
+    return new Promise(async (resolve, reject) => {
+      image.setFormat("png").toBuffer(function (error, stdout) {
+        if (error) return reject(error);
+        resolve(stdout);
+      });
+    });
+  }
+
+  matrixCalculator(corners, image) {
+    const perspectiveMatrix = {
+      tL: [0, 0, 0, 0],
+      tR: [image.width, 0, 0, 0],
+      bL: [0, image.height, 0, 0],
+      bR: [image.width, image.height, 0, 0],
+    };
+
+    Object.entries(corners).forEach((corner) => {
+      perspectiveMatrix[corner[0]][2] = corner[1].x;
+      perspectiveMatrix[corner[0]][3] = corner[1].y;
+    });
+
+    return Object.values(perspectiveMatrix)
+      .map((x) => x.join(","))
+      .join(" ");
+  }
+
+  async generatePerspective(image, layer, corners) {
+    const resizeOption = {
+      position: "center",
+      fit: "fill",
+      height: layer.height,
+      width: layer.width,
+    };
+
+    image = this.sharp(image).resize(resizeOption);
+
+    const perpectiveMatrix = this.matrixCalculator(corners, layer);
+
+    const distorted = this.gm(image)
+      .out("-matte")
+      .out("-virtual-pixel")
+      .out("transparent")
+      .out("-distort")
+      .out("Perspective")
+      .out(perpectiveMatrix);
+    return this.toPng(distorted);
+  }
+}
